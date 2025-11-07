5f0d62c674f74dcbe58b6f52175835fc2fc00b43
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-07 15:15:21 +0700

    menambahkan Formisian

--- app/src/main/java/com/example/questnavigasiui_038/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasiui_038/view/TampilanData.kt
@@ -1,2 +1,23 @@
 package com.example.questnavigasiui_038.view
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<R.string> = listOf("Laki-Laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
