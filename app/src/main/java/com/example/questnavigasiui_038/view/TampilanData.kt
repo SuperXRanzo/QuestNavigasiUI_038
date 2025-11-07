c0774b170831d354df4d6c0cf4be3c7b31e45dec
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-07 16:16:33 +0700

    mengubah isiian

--- app/src/main/java/com/example/questnavigasiui_038/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasiui_038/view/TampilanData.kt
@@ -25,50 +25,31 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
     )
 
-    Scaffold(
+    Scaffold (modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.tampil),
-                        color = Color.White
-                    )
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = colorResource(id = R.color.teal_700)
-                )
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
             )
-        }
-    ) { isiRuang ->
-
-        Column(
-            modifier = Modifier
-                .padding(paddingValues = isiRuang)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-
-            Column(
-                modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(
-                    space = dimensionResource(id = R.dimen.padding_small)
-                )
-            ) {
+        }){ isiRuang->
+        Column (modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = R.dimen.padding_small))) {
                 items.forEach { item ->
-                    Text(
-                        text = item.first.uppercase(),
-                        fontSize = 16.sp
-                    )
-                    Text(
-                        text = item.second,
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily.Cursive,
-                        fontSize = 22.sp
-                    )
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick) {
+                    Text(text = stringResource(id = R.string.back))
+                }
             }
-            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
