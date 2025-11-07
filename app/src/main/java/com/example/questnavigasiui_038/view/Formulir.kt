4638ea0baa4cacae2abd170fded68f502e6c6e10
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-07 15:36:21 +0700

    menambah OutlinedTextField untuk Alamat

--- app/src/main/java/com/example/questnavigasiui_038/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_038/view/Formulir.kt
@@ -3,8 +3,12 @@ package com.example.questnavigasiui_038.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -78,6 +82,21 @@ fun FormIsian(
                 thickness = 1.dp,
                 color = Color.Red
             )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = { Text(text = "ALamat") },
+                onValueChange = {},
+            )
+            Spacer(modifier = Modifier.height(height = 30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 1f)
+                    .padding(all = 25.dp),
+                onClick = OnSubmitBtnClick
+            ) {
+            }
         }
     }
 }
